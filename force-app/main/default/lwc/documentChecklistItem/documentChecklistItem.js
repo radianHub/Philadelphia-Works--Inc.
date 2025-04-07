8f5acb003d11644bd0fa4a6fb9ae785e11210f9d
Chloe Echikson <chloe.echikson@radianhub.com>
2025-04-07 02:17:10 +0000

    Added image preview ability, and can now always re-upload version even if it was already approved

--- force-app/main/default/lwc/documentChecklistItem/documentChecklistItem.js
+++ force-app/main/default/lwc/documentChecklistItem/documentChecklistItem.js
@@ -9,84 +9,81 @@ export default class DocumentChecklistItem extends LightningElement {
 	@api recordId;
 	@api checklistName;
 	@api lookupField;
-    @api reviewMode;
-    @api documentStatuses;
-    url;
-    selectedStatus;
-    isSubmitting;
-    rejectionReason;
+	@api reviewMode;
+	@api documentStatuses;
+	url;
+	selectedStatus;
+	isSubmitting;
+	rejectionReason;
 
-    documentOptions = [
-        {
-            label: 'Approve',
-            value: 'Approved'
-        }, {
-            label: 'Reject',
-            value: 'Rejected'
-        }
-    ]
+	documentOptions = [
+		{
+			label: 'Approve',
+			value: 'Approved',
+		},
+		{
+			label: 'Reject',
+			value: 'Rejected',
+		},
+	];
 
-    get statusIconName() {
-        switch (this.document.status) {
-            case this.documentStatuses.approvedStatus:
-                return 'utility:check';
-            case this.documentStatuses.rejectedStatus:
-                return 'utility:close';
-            case this.documentStatuses.submittedStatus:
-                return 'utility:search';
-            default:
-                return 'utility:cancel_file_request';
-        }
-    }
+	get statusIconName() {
+		switch (this.document.status) {
+			case this.documentStatuses.approvedStatus:
+				return 'utility:check';
+			case this.documentStatuses.rejectedStatus:
+				return 'utility:close';
+			case this.documentStatuses.submittedStatus:
+				return 'utility:search';
+			default:
+				return 'utility:cancel_file_request';
+		}
+	}
 
-    get statusIconVariant() {
-        switch (this.document.status) {
-            case this.documentStatuses.approvedStatus:
-                return 'success';
-            case this.documentStatuses.rejectedStatus:
-                return 'error';
-            case this.documentStatuses.submittedStatus:
-                return 'warning';
-            default:
-                return '';
-        }
-    }
+	get statusIconVariant() {
+		switch (this.document.status) {
+			case this.documentStatuses.approvedStatus:
+				return 'success';
+			case this.documentStatuses.rejectedStatus:
+				return 'error';
+			case this.documentStatuses.submittedStatus:
+				return 'warning';
+			default:
+				return '';
+		}
+	}
 
-    get statusIconText() {
-        return `${this.document.type} Status: ${this.document.status}`;
-    }
+	get statusIconText() {
+		return `${this.document.type} Status: ${this.document.status}`;
+	}
 
-    get statusBadgeClass() {
-        switch (this.document.status) {
-            case this.documentStatuses.approvedStatus:
-                return 'slds-theme_success slds-var-m-left_large';
-            case this.documentStatuses.rejectedStatus:
-                return 'slds-theme_error slds-var-m-left_large';
-            case this.documentStatuses.submittedStatus:
-                return 'slds-theme_warning slds-var-m-left_large';
-            default:
-                return 'slds-var-m-left_large';
-        }
-    }
+	get statusBadgeClass() {
+		switch (this.document.status) {
+			case this.documentStatuses.approvedStatus:
+				return 'slds-theme_success slds-var-m-left_large';
+			case this.documentStatuses.rejectedStatus:
+				return 'slds-theme_error slds-var-m-left_large';
+			case this.documentStatuses.submittedStatus:
+				return 'slds-theme_warning slds-var-m-left_large';
+			default:
+				return 'slds-var-m-left_large';
+		}
+	}
 
-    get isInReview() {
-        return this.document.status === this.documentStatuses.submittedStatus;
-    }
+	get isInReview() {
+		return this.document.status === this.documentStatuses.submittedStatus;
+	}
 
-    get canUpload() {
-        return this.document.status !== this.documentStatuses.approvedStatus;
-    }
+	get showRejectionReasonField() {
+		return this.selectedStatus === 'Rejected';
+	}
 
-    get showRejectionReasonField() {
-        return this.selectedStatus === 'Rejected';
-    }
-
-    render() {
-        if (this.reviewMode) {
-            return reviewTemplate;
-        }
-        return template;
-    }
+	render() {
+		if (this.reviewMode) {
+			return reviewTemplate;
+		}
+		return template;
+	}
 
 	handleUpload() {
 		const document = this.document;
@@ -114,70 +111,79 @@ export default class DocumentChecklistItem extends LightningElement {
 		);
 	}
 
-    handlePreview() {
-        getContentVersionFromDocumentId({ documentId: this.document.documentId })
-        .then((data) => {
-            let url;
-            const communityId = null;
+	handlePreview() {
+		getContentVersionFromDocumentId({ documentId: this.document.documentId })
+			.then((data) => {
+				let src;
+				const communityId = null;
 
-            if (communityId) {
-                url = `/sfsites/c/sfc/servlet.shepherd/document/download/${data.ContentDocumentId}`;
-            } else {
-                url = data.VersionDataUrl;
-            }
-            window.open(url);
-        })
-        .catch((error) => {
-            console.error('getContentVersionFromDocumentId error', error);
-        })
-    }
+				if (communityId) {
+					src = `/sfsites/c/sfc/servlet.shepherd/document/download/${data.ContentDocumentId}`;
+				} else {
+					src = data.VersionDataUrl;
+				}
 
-    handleApprove() {
-        this.dispatchEvent(
+				this.dispatchEvent(
+					new CustomEvent('preview', {
+						detail: {
+							title: this.document.name,
+							src: src,
+							fileExtension: data.FileExtension,
+						},
+					})
+				);
+			})
+			.catch((error) => {
+				console.error('getContentVersionFromDocumentId error', error);
+			});
+	}
+
+	handleApprove() {
+		this.dispatchEvent(
 			new CustomEvent('approve', {
 				detail: {
 					documentId: this.document.documentId,
 				},
 			})
 		);
-    }
+	}
 
-    handleReject() {
-        this.dispatchEvent(
+	handleReject() {
+		this.dispatchEvent(
 			new CustomEvent('reject', {
 				detail: {
 					documentId: this.document.documentId,
-                    rejectionReason: this.rejectionReason
+					rejectionReason: this.rejectionReason,
 				},
 			})
 		);
-    }
+	}
 
-    handleStatusChange(evt) {
-        this.selectedStatus = evt.detail.value;
-    }
+	handleStatusChange(evt) {
+		this.selectedStatus = evt.detail.value;
+	}
 
-    handleRejectionReasonChange(evt) {
-        this.rejectionReason = evt.target.value;
-    }
+	handleRejectionReasonChange(evt) {
+		this.rejectionReason = evt.target.value;
+	}
 
-    handleSubmit() {
-        this.isSubmitting = true;
+	handleSubmit() {
+		this.isSubmitting = true;
 
-        const allValid = [
-            ...this.template.querySelectorAll('lightning-input'),
-            ...this.template.querySelectorAll('lightning-radio-group')
-        ].reduce((validSoFar, inputCmp) => {
-            inputCmp.reportValidity();
-            return validSoFar && inputCmp.checkValidity();
-        }, true);
+		const allValid = [
+			...this.template.querySelectorAll('lightning-input'),
+			...this.template.querySelectorAll('lightning-radio-group'),
+		].reduce((validSoFar, inputCmp) => {
+			inputCmp.reportValidity();
+			return validSoFar && inputCmp.checkValidity();
+		}, true);
 
-        if (allValid) {
-            if (this.selectedStatus === 'Approved') {
-                this.handleApprove();
-            } else if (this.selectedStatus === 'Rejected') {
-                this.handleReject();
-            }
-        }
-    }
+		if (allValid) {
+			if (this.selectedStatus === 'Approved') {
+				this.handleApprove();
+			} else if (this.selectedStatus === 'Rejected') {
+				this.handleReject();
+			}
+		}
+	}
 }
